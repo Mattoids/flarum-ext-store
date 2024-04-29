be983d2c947c29b3fc0f3fa29cc500d3f6d50c44
mattoid <liufei@liufei.info>
2024-04-29 13:34:58 +0800

    add

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -1,6 +1,24 @@
 import app from 'flarum/admin/app';
-import StatisticsPage from './components/StatisticsPage';
 
 app.initializers.add('mattoid-store', () => {
-  app.extensionData.for("mattoid-store").registerPage(StatisticsPage)
+  app.extensionData.for("mattoid-store")
+    .registerSetting({
+      setting: 'mattoid-store.storeName',
+      help: app.translator.trans('mattoid-store.admin.settings.store-name-requirement'),
+      label: app.translator.trans('mattoid-store.admin.settings.store-name'),
+      type: 'string',
+    })
+    .registerSetting({
+      setting: 'mattoid-store.decorationStoreTimezone',
+      help: app.translator.trans('mattoid-store.admin.settings.decorationStoreTimezone-requirement'),
+      label: app.translator.trans('mattoid-store.admin.settings.decorationStoreTimezone'),
+      type: 'string',
+    })
+    .registerPermission(
+      {
+        icon: 'fas fa-id-card',
+        label: app.translator.trans('mattoid-store.admin.settings.group-view'),
+        permission: 'mattoid-store.group-view',
+        allowGuest: true
+      }, 'view')
 });
--- js/src/forum/demo.ts
+++
@@ -1,7 +0,0 @@
-import Goods from "../common/Goods";
-
-export default class demo extends Goods {
-  protected name : string = "demo"
-  protected describe: string = "demo goods"
-
-}
