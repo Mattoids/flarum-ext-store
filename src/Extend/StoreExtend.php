6ef6d62cbe1ddd7c476c98e1e7ab1c26c86220ce
mattoid <liufei@liufei.info>
2024-06-25 16:36:40 +0800

    增加基础功能

--- src/Extend/StoreExtend.php
+++ src/Extend/StoreExtend.php
@@ -5,13 +5,21 @@ namespace Mattoid\Store\Extend;
 use Flarum\Extend\ExtenderInterface;
 use Flarum\Extend\LifecycleInterface;
 use Flarum\Extension\Extension;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Container\Container;
 use Mattoid\Store\Model\StoreCommodityModel;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class StoreExtend implements ExtenderInterface, LifecycleInterface
 {
 
     protected $goodList = [];
+
+    public function __construct()
+    {
+        $this->settings = resolve(SettingsRepositoryInterface::class);
+        $this->translator = resolve(TranslatorInterface::class);
+    }
 
     public function addStoreGoods(string $key, $callback): self
     {
@@ -26,10 +34,17 @@ class StoreExtend implements ExtenderInterface, LifecycleInterface
 
     public function onEnable(Container $container, Extension $extension)
     {
-        app('log')->info($extension->type);
-        $storeCommodity = new StoreCommodityModel();
-        $storeCommodity->code = $extension->name;
-        $storeCommodity->save();
+        $key = $extension->name;
+        $class = $this->goodList[$key];
+        $commodity = new $class();
+
+        StoreCommodityModel::query()->insert([
+            'code' => $commodity->code,
+            'name' => $commodity->name,
+            'uri' => $commodity->uri,
+            'pop_up' => $commodity->popUp,
+            'create_time' => Date("Y-m-d H:i:s")
+        ]);
     }
 
     public function onDisable(Container $container, Extension $extension)
