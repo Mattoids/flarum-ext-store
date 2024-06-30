cb8f4be97d3cdc4199d5e9ed6502b04fa13856a7
mattoid <liufei@liufei.info>
2024-06-30 19:48:32 +0800

    修改 前置、后置出来逻辑

--- src/Controller/BuyGoodsController.php
+++ src/Controller/BuyGoodsController.php
@@ -117,8 +117,8 @@ class BuyGoodsController extends AbstractListController
         $after = StoreExtend::getAfter($store->code);
         if ($after) {
             // 商品处理失败，通知购买失败事件进行回滚操作
-            if (!$after->after($actor, $store, $params)) {
-                $this->events->dispatch(new StoreBuyFailEvent($user, $store, $cart, $params, $this->translator, $this->settings, $this->events, $this->cache));
+            if (!$after->after($actor, $store, $params, $this->translator, $this->settings, $this->events, $this->cache)) {
+                $this->events->dispatch(new StoreBuyFailEvent($user, $store, $cart, $params));
                 throw new ValidationException(['message' => $this->translator->trans('mattoid-store.forum.error.buy-goods-fail', ['title' => $store->title])]);
             }
         }
