3816757272231a8575cb99a773bfd93223b0e3ee
mattoid <liufei@liufei.info>
2024-07-19 23:42:52 +0800

    增加商品使用逻辑

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -25,7 +25,7 @@ app.initializers.add('mattoid/store', () => {
       href: app.route('myCartPage', {
         username: this.user.slug(),
       }),
-      icon: 'fas fa-check-square',
+      icon: 'fas fa-shop',
     }, app.translator.trans('mattoid-store.forum.cart')));
   });
 
